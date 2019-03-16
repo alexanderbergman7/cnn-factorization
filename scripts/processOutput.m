f4666e51c17c160c271bef81f2de5bd9cd6e592b
Alex Bergman <alexbergman@DN525eol.SUNet>
2019-03-15 18:24:12 -0700

    output processing script

--- scripts/processOutput.m
+++ scripts/processOutput.m
@@ -2,21 +2,21 @@ output = load('../outputIm.mat');
 outputF = load('../filenames.mat');
 images = output.outputIm;
 filenames = outputF.filenames;
-validation = dir('../../dataset/images/val_gray/*.jpg');
 
 for i = 1:size(images,1)
-   assert(all(strcat(filenames(i,:)) == validation(i).name))
-   filename = validation(i).name;
+   filename = strcat(filenames(i,:));
    
    imageR = im2double(uint8(squeeze(images(i,:,:))));
    image = im2double(imread(strcat('../../dataset/images/val_gray/',filename)));
    
    MSE = (1/(size(image,1)*size(image,2)))*sum(sum((image-imageR).^2));
    PSNR = 10*log10((max(image(:))^2)/MSE);
+   SSIM = ssim(imageR, image);
    
    val_results(i).name = filename;
    val_results(i).PSNR = PSNR;
    val_results(i).MSE = MSE;
+   val_results(i).SSIM = SSIM;
 end
 
-save('../val_results_E25_BS40_ic1_oc1_f1_d2.mat', 'val_results')
+save('../test_model_env/val_results.mat', 'val_results')
