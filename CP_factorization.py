2ea99255a2b86f07cde11843a2f3896e7bb883d2
Alex Bergman <alexbergman@DN525eol.SUNet>
2019-03-15 17:47:51 -0700

    cp factorization complete

--- CP_factorization.py
+++ CP_factorization.py
@@ -27,16 +27,27 @@ import scipy.io as sio
 
 import tensorly.decomposition as d
 
-USE_GPU = True
-USE_CROP = False
+import constants
 
+USE_GPU = constants.USE_GPU
+USE_CROP = constants.USE_CROP
+CROP_SIZE = constants.CROP_SIZE
+IMAGE_SIZE = constants.IMAGE_SIZE
+
+if USE_GPU:
+	print(torch.cuda.is_available())
+	torch.cuda.set_device(0)
+
+# class used for interfacing the dataset
 class Dataset():
 
+	# initialize the dataset based on the directory that images will be contained in
 	def __init__(self, root_dir):
 		self.root_dir = root_dir
 		self.train = os.listdir(os.path.join(root_dir, 'train_gray'))
 		self.val = os.listdir(os.path.join(root_dir, 'val_gray'))
 
+	# get a training pair in tensor form
 	def getTrainItem(self, idx):
 		image_original = torch.from_numpy(io.imread(os.path.join(self.root_dir, 'train_gray', self.train[idx])))
 		image_noisy = torch.from_numpy(io.imread(os.path.join(self.root_dir, 'noisy_train_sigma01', self.train[idx])))
@@ -45,6 +56,7 @@ class Dataset():
 
 		return image_original, image_noisy
 
+	# get a validation/testing pair in tensor form
 	def getValItem(self, idx):
 		image_original = torch.from_numpy(io.imread(os.path.join(self.root_dir, 'val_gray', self.val[idx])))
 		image_noisy = torch.from_numpy(io.imread(os.path.join(self.root_dir, 'noisy_val_sigma01', self.val[idx])))
@@ -53,6 +65,7 @@ class Dataset():
 
 		return image_original, image_noisy
 
+	# get a batch of training data, either cropped or not
 	def getTrainingDataBatch(self, start, size):
 		images_orig = []
 		images_noisy = []
@@ -60,7 +73,7 @@ class Dataset():
 			image_original, image_noisy = self.getTrainItem(i)
 
 			if USE_CROP:
-				image_original, image_noisy = self.transformCrop(image_original, image_noisy, 32, 32)
+				image_original, image_noisy = self.transformCrop(image_original, image_noisy, CROP_SIZE[0], CROP_SIZE[1])
 
 			images_orig.append(image_original)
 			images_noisy.append(image_noisy)
@@ -72,9 +85,10 @@ class Dataset():
 
 		return images_orig, images_noisy
 
+	# crop two tensor images
 	def transformCrop(self, image_original, image_noisy, th, tw):
-		cols = 480
-		rows = 320
+		cols = IMAGE_SIZE[1]
+		rows = IMAGE_SIZE[0]
 		if cols == tw and rows == th:
 			i = 0
 			j = 0
@@ -87,6 +101,7 @@ class Dataset():
 
 		return image_original, image_noisy
 
+	# get a batch of validation data
 	def getValidationDataBatch(self, start, size):
                 images_orig = []
                 images_noisy = []
@@ -103,35 +118,48 @@ class Dataset():
 
                 return images_orig, images_noisy
 
+    # get the length of the training data
 	def trainingLength(self):
 		return len(self.train)
 
+	# get the length of the validation data
 	def validationLength(self):
 		return len(self.val)
 
+	# get validation filenames
 	def validationFiles(self):
 		return self.val
 
-# Get 2D UNet model, single channel input, output single channel, features at top level of denoised image
-# params: input channels, output channels, number of top level features, down/up-samples, maximum features, use dropout
-UNet2D = torch.load('final_model_UNet.pt')
+# Load 2D UNet model which has already been trained
+UNet2D = torch.load('model.pt')
 if USE_GPU:
 	UNet2D = UNet2D.cuda()
+else:
+	UNet2D = UNet2D.cpu()
 
+# keep dictionary of modules to replace
 modules = {}
+
+# iterate through layers
 for name, layer in UNet2D.named_modules():
 	if isinstance(layer, nn.Conv2d):
-		last, first, vertical, horizontal = d.parafac(layer.weight.data.cpu().numpy(), rank=6, init='svd')
+		# code based off of https://jacobgil.github.io/deeplearning/tensor-decompositions-deep-learning
 
+		# perform CP decomposition on layer if it is a convolutional layer
+		last, first, vertical, horizontal = d.parafac(layer.weight.data.cpu().numpy(), rank=constants.rank, init='svd')
+
+		# take input pointwise layer and set up as pointwise conv2d with 1x1 kernel
 		pointwise_s_to_r_layer = torch.nn.Conv2d(in_channels=first.shape[0], \
 	            out_channels=first.shape[1], kernel_size=1, stride=1, padding=0, 
 	            dilation=layer.dilation, bias=False)
 
+		# implement depthwise vertical layer as a grouped convolution, preserve the stride of the original convolution
 		depthwise_vertical_layer = torch.nn.Conv2d(in_channels=vertical.shape[1], 
 	            out_channels=vertical.shape[1], kernel_size=(vertical.shape[0], 1),
 	            stride=1, padding=(layer.padding[0], 0), dilation=layer.dilation,
 	            groups=vertical.shape[1], bias=False)
 
+		# implement depthwise horizontal layer as a grouped convolution, preserve the stride of the original convolution
 		depthwise_horizontal_layer = \
 	        torch.nn.Conv2d(in_channels=horizontal.shape[1], \
 	            out_channels=horizontal.shape[1], 
@@ -139,15 +167,24 @@ for name, layer in UNet2D.named_modules():
 	            padding=(0, layer.padding[0]), 
 	            dilation=layer.dilation, groups=horizontal.shape[1], bias=False)
 
+	    # iplement pointwise layer as conv2d with 1x1 kernel also
 		pointwise_r_to_t_layer = torch.nn.Conv2d(in_channels=last.shape[1], \
 	            out_channels=last.shape[0], kernel_size=1, stride=1,
-	            padding=0, dilation=layer.dilation, bias=True)
+	            padding=0, dilation=layer.dilation, bias=layer.bias)
 
-		horizontal = torch.from_numpy(horizontal).type('torch.FloatTensor').cuda()
-		vertical = torch.from_numpy(vertical).type('torch.FloatTensor').cuda()
-		first = torch.from_numpy(first).type('torch.FloatTensor').cuda()
-		last = torch.from_numpy(last).type('torch.FloatTensor').cuda()
+		# convert if using GPU
+		if USE_GPU:
+			horizontal = torch.from_numpy(horizontal).type('torch.FloatTensor').cuda()
+			vertical = torch.from_numpy(vertical).type('torch.FloatTensor').cuda()
+			first = torch.from_numpy(first).type('torch.FloatTensor').cuda()
+			last = torch.from_numpy(last).type('torch.FloatTensor').cuda()
+		else:
+			horizontal = torch.from_numpy(horizontal).type('torch.FloatTensor')
+			vertical = torch.from_numpy(vertical).type('torch.FloatTensor')
+			first = torch.from_numpy(first).type('torch.FloatTensor')
+			last = torch.from_numpy(last).type('torch.FloatTensor')
 
+		# initialize weights of the layers constructed with the tensors obtained from the CP decomposition
 		depthwise_horizontal_layer.weight.data = \
 	        torch.transpose(horizontal, 1, 0).unsqueeze(1).unsqueeze(1)
 		depthwise_vertical_layer.weight.data = \
@@ -156,11 +193,15 @@ for name, layer in UNet2D.named_modules():
 	        torch.transpose(first, 1, 0).unsqueeze(-1).unsqueeze(-1)
 		pointwise_r_to_t_layer.weight.data = last.unsqueeze(-1).unsqueeze(-1)
 
+		# new layers to replace the existing layer
 		new_layers = [pointwise_s_to_r_layer, depthwise_vertical_layer, \
 	                    depthwise_horizontal_layer, pointwise_r_to_t_layer]
 	    
+	    # update the module reference in this named layer
 		modules[name] = nn.Sequential(*new_layers)
 
+# replace the modules with "name" with what is in the dictionary, based on code found at
+# https://discuss.pytorch.org/t/change-all-conv2d-and-batchnorm2d-to-their-3d-counterpart/24780
 for name in modules:
     parent_module = UNet2D
     objs = name.split(".")
@@ -173,7 +214,10 @@ for name in modules:
 
     parent_module.__setattr__(objs[-1], modules[name])
 
-UNet2D = UNet2D.cuda()
+if USE_GPU:
+	UNet2D = UNet2D.cuda()
+else:
+	UNet2D = UNet2D.cpu()
 
 # Create dataset representation
 d = Dataset('../dataset/images')
@@ -185,20 +229,17 @@ if USE_GPU:
 	valDataX = valDataX.cuda()
 
 # save model, get output for validation data
-torch.save(UNet2D, 'test_model_CP_rT.pt')
+torch.save(UNet2D, 'CP_decomposed_model.pt')
 
 output = UNet2D(valDataX)
-print(output.size())
 
 # write this data to be post-processed for MSE/PSNR values
 output = torch.squeeze(output)
 if USE_GPU:
 	output = output.cpu()
 
+# save output images and filenames
 outputIm = output.detach().numpy()
-sio.savemat('outputIm_CPr6.mat', {'outputIm': outputIm})
+sio.savemat('outputIm_CP.mat', {'outputIm': outputIm})
 val = d.validationFiles()
-sio.savemat('filenames_CPr1.mat', {'filenames': val})
-
-#process = psutil.Process(os.getpid())
-#print(process.memory_info().rss)
+sio.savemat('filenames_CP.mat', {'filenames': val})
---
+++ constants.py
@@ -0,0 +1,12 @@
+USE_GPU = False
+USE_CROP = False
+CROP_SIZE = [32, 32]
+IMAGE_SIZE = [320, 480]
+EPOCHS = 3
+BatchSize = 40
+learning_rate = 0.01
+input_channels = 1
+output_channels = 1
+highest_level_features = 1
+depth = 1
+rank = 1
