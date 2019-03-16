74e3582392a0b4ac0c0f712a6f6c58e4a6a8b0ab
Alex Bergman <alexbergman@DN0a1ea67a.SUNet>
2019-03-15 22:26:53 -0700

    bug in script assuming incorrect directory structure

--- cnn-factorization/scripts/BM3D_denoising.m
+++ cnn-factorization/scripts/BM3D_denoising.m
@@ -1,16 +1,16 @@
 % in order for this script to run, must download the BM3D codebase at 
 % http://www.cs.tut.fi/~foi/GCF-BM3D/ and add the BM3D function to the
 % filepath
-validation = dir('../dataset/images/val_gray/*.jpg');
-validationNoisy = dir('../dataset/images/noisy_val_sigma01/*.jpg');
+validation = dir('../../dataset/images/val_gray/*.jpg');
+validationNoisy = dir('../../dataset/images/noisy_val_sigma01/*.jpg');
 
 nvals = length(validation);
 
 % loop through validation files/testing files to evaluate with BM3D
 for i=1:nvals
    filename = validation(i).name;
-   image = im2double(imread(strcat('../dataset/images/val_gray/',filename)));
-   image_noisy = im2double(imread(strcat('../dataset/images/noisy_val_sigma01/',filename)));
+   image = im2double(imread(strcat('../../dataset/images/val_gray/',filename)));
+   image_noisy = im2double(imread(strcat('../../dataset/images/noisy_val_sigma01/',filename)));
    
    % calculate PSNR of BM3D denoised image
    [PSNR, est] = BM3D(image, image_noisy, .1*255, 'np', 0);
@@ -21,7 +21,7 @@ for i=1:nvals
    val_results(i).PSNR = PSNR;
    val_results(i).MSE = MSE;
    
-   imwrite(est, strcat('../dataset/images/noisy_val_sigma01_BM3D_denoised/', filename));
+   imwrite(est, strcat('../../dataset/images/noisy_val_sigma01_BM3D_denoised/', filename));
 end
 
-save('../dataset/images/noisy_val_sigma01_BM3D_denoised/val_results.mat', 'val_results')
+save('../../dataset/images/noisy_val_sigma01_BM3D_denoised/val_results.mat', 'val_results')
